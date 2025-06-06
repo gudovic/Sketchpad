3d5a597938a77a68c3158e416d2722562496ce48
maxmax <174373891+gudovic@users.noreply.github.com>
2025-06-06 11:36:09 +0200

    everything in one swoop

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Sketchpad
+
+1. Made everything in one big function, will make smaller more managable functions in next project
+2. It was very fun figuring out how to scale the board with different sizes of the grid. 
+3. 
--- javascript.js
+++ javascript.js
@@ -1,21 +1,55 @@
 const board = document.getElementById('board');
-const clear = document.getElementById('clear');
+let mouseDown = false;
+document.body.addEventListener('pointerdown', () => mouseDown = true);
+document.body.addEventListener('pointerup', () => mouseDown = false);
 
 
-function makeGrid() {
-    for (let i=0; i<256; i++) {
-     const div = document.createElement('div');
-        div.classList.add('grid');
-        board.appendChild(div);
-    
-        div.addEventListener('mousedown', () => {
-        div.style.backgroundColor = 'black'
-        })
+function makeGrid(scale) {
+	board.innerHTML = "";
+	const setSize = scale*scale;
+  
+	for (let i=0; i<setSize; i++) {
+  	const squares = document.createElement('div');
+    board.append(squares);
+    squares.classList.add('grid');
+    squares.isWhite = true;
+    // 400 is the boardheight i should probably declare it and not hardcode it
+    squares.style.height = 400/scale + 'px';
+    squares.style.width = 400/scale + 'px';
+
+    squares.addEventListener('pointerdown', () => {
+      if (squares.isWhite == true) {
+      	squares.style.backgroundColor = 'black';
+        squares.isWhite = false;
+      } else {squares.style.backgroundColor = 'white';
+      				squares.isWhite = true;
+       }
+    });
+    squares.addEventListener('pointerenter', () => {
+  	if (mouseDown) {
+      if (squares.isWhite == true) {
+      	squares.style.backgroundColor = 'black';
+        squares.isWhite = false;
+      } else {squares.style.backgroundColor = 'white';
+      				squares.isWhite = true;
+       }
     }
-}
-makeGrid();
+});
+  }
+} 
+makeGrid(16);
 
-clear.addEventListener('click', () => {
+document.getElementById('button-small').addEventListener('pointerdown', () => {
+    		makeGrid(16);
+    })
+document.getElementById('button-mid').addEventListener('pointerdown', () => {
+    		makeGrid(32);
+    })
+document.getElementById('button-big').addEventListener('pointerdown', () => {
+    		makeGrid(64);
+    })
     
-})
-    
+document.getElementById('clear').addEventListener('pointerdown', () => {
+			const squares =	document.querySelectorAll('.grid')
+      squares.forEach(square => square.style.backgroundColor = 'white');
+    })
